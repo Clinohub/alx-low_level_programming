edb15b0e55a37e7868704cff0a67ac0bde0d97d0
Clinohub <esmicorreo09@gmail.com>
2022-12-12 21:37:15 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,7 @@ int main(void)
 	printf("Size of char: %zu byte\n", sizeof(charType));
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of long int: %zu bytes\n", sizeof(longintType));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType);
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	return (0);
 }
