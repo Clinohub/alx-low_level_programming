faaf2ca46199476019f4aeed2043b7b5071493cf
Clinohub <esmicorreo09@gmail.com>
2022-12-15 20:40:02 -0800

    function_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit
+ * print_sign - prints the last digit
  * @n: the number
  * Return: value of the last digit
  */
 
-int print_last_digit(int n)
+int print_sign (int n)
 {
 
 	int ls = n % 10;
