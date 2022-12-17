311aa02dffdd827fda5f0129304d8f0d89c0bb71
Clinohub <esmicorreo09@gmail.com>
2022-12-17 13:24:02 +0300

    nested_more

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,23 +1,14 @@
 #include "main.h"
 
-
-
 /**
- *
  * mul - multiplying 2 integers
- *
  * @a: parameter 1
- *
  * @b: parameter 2
  * Return: successful evaluation
  */
 
-
-
 int mul(int a, int b)
 
 {
-
 	        return (a * b);
-
 }
