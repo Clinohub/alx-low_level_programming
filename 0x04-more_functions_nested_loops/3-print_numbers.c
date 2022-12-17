6092958cf218774c3db83580b3ecf850b71c79aa
Clinohub <esmicorreo09@gmail.com>
2022-12-17 13:28:35 +0300

    nested_more

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,22 +1,13 @@
-i#include "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_numbers - prints the numbers, from 0 to 9
- *
- *   */
-
-
+ * print_numbers - prints the numbers, from 0 to 9
+ */
 
 void print_numbers(void)
 
 {
-
 	        int n;
-
-
 
 	        for (n = 0; n < 10; n++)
 		       	_putchar(x + '0');
