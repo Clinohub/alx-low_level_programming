aba7005df13821b8383f67306ac24e33ee72ad1b
Clinohub <esmicorreo09@gmail.com>
2022-12-18 13:23:14 -0800

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char num_16;
+	
+	for (num_16 = '0'; num_16 <= '9'; num_16++)
+		putchar(num_16);
+
+	for (num_16 = 'a'; num_16 <= 'f'; num_16++)
+		putchar(num_16);
+
+	putchar('\n');
+
+	return (0);
+}
