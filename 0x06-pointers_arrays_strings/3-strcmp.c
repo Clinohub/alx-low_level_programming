f97c804087a57b5a50a3b3bea5f3ebf057fe087a
Clinohub <esmicorreo09@gmail.com>
2022-12-22 12:23:48 -0800

    1

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * s2: string two
+ * Return :int value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int alpha;
+	
+	while (s1[a])
+		a++;
+	
+	while (s2[b])
+		b++;
+
+	if (a <= b)
+		lim = a;
+
+	else
+		lim = b;
+
+	while (c <= alpha)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			d = s1[c] - s2[cbreak];
+		}
+		
+		c++
+	}	
+		
+		return (r);
+	}
+}
