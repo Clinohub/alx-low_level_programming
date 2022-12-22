cf1cf6ade4940cd9ed9abc344c72bf5ed9b2df05
Clinohub <esmicorreo09@gmail.com>
2022-12-22 11:49:34 -0800

    f

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *_strncpy -  copies a string
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The copy limit
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
