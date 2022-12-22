8dc7b32e7689287fce3c9cb3014be8d9f02bcc44
Clinohub <esmicorreo09@gmail.com>
2022-12-22 10:36:08 -0800

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 /**
  * *_strcat - function that concatenates two strings.
@@ -11,7 +10,6 @@
 char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
-	
 
 	return (dest);
 }
