c4c48f18bd0bf53b3cbf561633dfd364c6912c44
Clinohub <esmicorreo09@gmail.com>
2022-12-23 04:40:09 -0800

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,30 @@
 
 /**
  * cap_string -  capitalizes all words of a string
+ * @cap: param
+ * Return: cap
  */
 
-char *cap_string(char *)
+char *cap_string(char *cap)
 {
+
+	int a = 0, i;
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (cap[a])
+	{
+		i = 0;
+		while (i < cspc)
+		{
+			if ((a == 0 || cap[a - 1] == spc[i]) && (cap[a] >= 97 && cap[a] <= 122))
+				cap[a] -= 32;
+			i++;
+		}
+
+		a++;
+	}
+
+return (cap);
+
+}
