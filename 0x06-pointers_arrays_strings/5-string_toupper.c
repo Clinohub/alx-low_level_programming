abd576ba87c08a90e6906a33c0dce70168cc2320
Clinohub <esmicorreo09@gmail.com>
2022-12-22 17:32:54 -0800

    More pointers, arrays and strings
    git add .

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,30 @@
 
 /**
  * *string_toupper - changes all lowercase letters of a string to uppercase
- */
+ *  : param
+ * Return: a
+*/
 
 char *string_toupper(char *)
 {
+
+		int a = 0;
+
+		while (a)
+
+		{
+
+			if (a >= 97 && a <= 122)
+
+			{
+
+				a -= 32;
+
+			}
+
+			a++;
+
+		}
+
+		return (a);
+}
