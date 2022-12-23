4bfd19f85bf8426c72f2f86608364cad624e5efc
Clinohub <esmicorreo09@gmail.com>
2022-12-22 17:12:22 -0800

    1

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: An array of integers
+ * @n: Number of elements to swap
+ */
+
+void reverse_array(int *a, int n)
+{
