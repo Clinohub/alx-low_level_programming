d7991cbd24eaa2734ec7b7bbda60b55df7c9541f
Clinohub <esmicorreo09@gmail.com>
2023-01-12 09:47:02 -0800

    malloc_free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
-  * free_grid - frees a 2 dimensional grid previously created by your alloc_grid
+  * free_grid - frees a 2 dimensional grid
+  * previously created by your alloc_grid
   * @grid: grid to be freed
   * @height: height of the grid
   * Return: 0
