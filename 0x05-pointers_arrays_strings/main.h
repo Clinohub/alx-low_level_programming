5fe40e359da286dae4474e3195f0f8db6ca77672
Clinohub <esmicorreo09@gmail.com>
2023-01-24 15:47:21 +0300

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,9 +10,12 @@ void print_rev(char *s);
 void rev_string(char *s);
 
 int _strlen(char *s);/*Function in 5-rev_string*/
+
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+/*ADVANCED*/
+int _atoi(char *s);
 
 #endif/*MAIN_H*/
